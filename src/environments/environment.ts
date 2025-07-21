46bac27f223df3111a6b654f4bcb355def6cfe65
arek-lab <arkadiusz.pawlak01@gmail.com>
2025-07-21 09:07:08 +0200

    api url changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api/v1/',
-  // apiUrl: 'https://rag-explorer-f551d45fdadb.herokuapp.com/api/v1/',
+  // apiUrl: 'http://localhost:5000/api/v1/',
+  apiUrl: 'https://rag-explorer-f551d45fdadb.herokuapp.com/api/v1/',
 };
